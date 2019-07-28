ddae6354bce26c5acd27c42c3561df0e554802fb
Gaut9a8m <gaut9a8m@gmail.com>
2019-07-28 13:59:53 +0530

    using router

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,12 @@
-from django.conf.urls import url
+from django.conf.urls import url,include
 from . import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('product',views.productView)
 
 urlpatterns = [
-    url(r'^product/$',views.product_list),
-    url(r'^product/(?P<pk>\d+)/$',views.product_detail),
+    # url(r'^product/$',views.product_list),
+    # url(r'^product/(?P<pk>\d+)/$',views.product_detail),
+    url('',include(router.urls))
 ]
